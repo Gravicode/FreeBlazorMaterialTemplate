7e00885896efa9e6ca1f55531dc3001023fa63a1
mifmasterz <mifmasterz@yahoo.com>
2022-03-01 20:56:15 +0700

    init

---
+++ MaterialBlazor/MaterialBlazor/Data/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace MaterialBlazor.Data
+{
+    public class AppConfig
+    {
+        //public enum ThemeStyles
+        //{
+        //    Dark,Light
+        //}
+        //public static ThemeStyles CurrentTheme = ThemeStyles.Light;
+        public static string AppName = "Material Blazor Template";
+    }
+}
